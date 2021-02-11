44a6bf0192f9479ac4ee199244920b7bfe0e9526
Ben <benkj235@gmail.com>
2021-02-10 20:25:15 -0800

    added overlord skeleton API

---
+++ src/test/java/com/onserv/overlord/OverlordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onserv.overlord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverlordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
